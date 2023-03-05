5191a7a6d19dee3596302491b12a1b475e9d66ee
nicu <lino479Nick@mail.ru>
2023-03-05 23:51:10 +0300

    Changed auth transport to Bearer Token

--- src/auth/instance.py
+++ src/auth/instance.py
@@ -2,30 +2,21 @@ from beanie import PydanticObjectId
 from fastapi_users import FastAPIUsers
 from fastapi_users.authentication import (
     AuthenticationBackend,
-    JWTStrategy,
-    CookieTransport,
+    BearerTransport,
+    JWTStrategy
 )
 
-from src.config import DEBUG, COOKIE_DOMAIN
 from .models import User
 from .manager import get_user_manager
 
-cookie_transport = CookieTransport(
-    cookie_name="mayaLoh",
-    cookie_httponly=False,
-    cookie_secure=True if DEBUG == "False" else False,
-    cookie_samesite="none" if DEBUG == "False" else "lax",
-    cookie_max_age=3600,
-    cookie_domain=COOKIE_DOMAIN
-)
-
+bearer_transport = BearerTransport(tokenUrl='auth/jwt/login')
 
 def get_jwt_strategy():
     return JWTStrategy(secret="SECRET", lifetime_seconds=3600)
 
 
 backend = AuthenticationBackend(
-    name="jwt", transport=cookie_transport, get_strategy=get_jwt_strategy
+    name="jwt", transport=bearer_transport, get_strategy=get_jwt_strategy
 )
 
 fastapi_users = FastAPIUsers[User, PydanticObjectId](get_user_manager, [backend])
